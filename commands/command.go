0793f0c3486b299410028a9ad91f04f25d1b87bd
twystd <twystd@users.noreply.github.com>
2023-03-21 12:07:01 -0700

    Fixed typo in Makefile

--- commands/command.go
+++ commands/command.go
@@ -1,10 +1,10 @@
 package commands
 
 import (
-	"flag"
-	"fmt"
+//	"flag"
+//	"fmt"
 
-	"github.com/uhppoted/uhppoted-app-db/log"
+// "github.com/uhppoted/uhppoted-app-db/log"
 )
 
 const APP = "uhppoted-app-db"
@@ -14,25 +14,25 @@ type Options struct {
 	Debug  bool
 }
 
-func helpOptions(flagset *flag.FlagSet) {
-	count := 0
-	flag.VisitAll(func(f *flag.Flag) {
-		count++
-	})
+//func helpOptions(flagset *flag.FlagSet) {
+//	count := 0
+//	flag.VisitAll(func(f *flag.Flag) {
+//		count++
+//	})
+//
+//	flagset.VisitAll(func(f *flag.Flag) {
+//		fmt.Printf("    --%-13s %s\n", f.Name, f.Usage)
+//	})
+//
+//	if count > 0 {
+//		fmt.Println()
+//		fmt.Println("  Options:")
+//		flag.VisitAll(func(f *flag.Flag) {
+//			fmt.Printf("    --%-13s %s\n", f.Name, f.Usage)
+//		})
+//	}
+//}
 
-	flagset.VisitAll(func(f *flag.Flag) {
-		fmt.Printf("    --%-13s %s\n", f.Name, f.Usage)
-	})
-
-	if count > 0 {
-		fmt.Println()
-		fmt.Println("  Options:")
-		flag.VisitAll(func(f *flag.Flag) {
-			fmt.Printf("    --%-13s %s\n", f.Name, f.Usage)
-		})
-	}
-}
-
-func infof(format string, args ...any) {
-	log.Infof(format, args...)
-}
+// func infof(format string, args ...any) {
+// 	log.Infof(format, args...)
+// }
